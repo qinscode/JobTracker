19d860d6a2b6bc4d838790a710fb0de200c197f9
qinscode <23992836@student.uwa.edu.au>
2024-09-29 21:01:23 +0800

    ui(theme): switch default theme from dark to light
    Change the default theme in the application from dark to light while maintaining the
    same storage key for consistency. This adjustment offers a fresh visual experience to users
    preferring a lighter interface without affecting the theme persistence functionality.

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ console.error = function (message, ...args) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
         <Toaster />
       </ThemeProvider>
